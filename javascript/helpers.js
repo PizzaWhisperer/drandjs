1fa7dcd496c86276c535769d3d80d4d13e03888d
PizzaWhisperer <mathilde.raynal@epfl.ch>
2019-05-09 17:02:52 +0200

    getting round involve

--- javascript/helpers.js
+++ javascript/helpers.js
@@ -1,3 +1,4 @@
+//fetches the randomness
 function fetchPublic(identity) {
   var fullPath = identity.Address + "/public";
   if (identity.TLS == false) {
@@ -8,6 +9,7 @@ function fetchPublic(identity) {
   return fetch(fullPath).then(resp => Promise.resolve(resp.json()));
 }
 
+//fetches the public key
 function fetchKey(identity) {
   var fullPath = identity.Address + "/info/dist_key";
   if (identity.TLS == false) {
@@ -18,26 +20,46 @@ function fetchKey(identity) {
   return fetch(fullPath).then(resp => Promise.resolve(resp.json()));
 }
 
+function bytesToHex(bytes) {
+		for (var hex = [], i = 0; i < bytes.length; i++) {
+			hex.push((bytes[i] >>> 4).toString(16));
+			hex.push((bytes[i] & 0xF).toString(16));
+		}
+		return hex.join("");
+	}
+
+//hex string ti bytes array
 function hexToBytes(hex) {
     for (var bytes = [], c = 0; c < hex.length; c += 2)
     bytes.push(parseInt(hex.substr(c, 2), 16));
     return bytes;
 }
 
-function message(msg, round) {
-/*var buff bytes.Buffer
-binary.Write(&buff, binary.BigEndian, round)
-buff.Write(prevRand)
-return buff.Bytes()*/
+//from int to bytes
+function intToBytes(int) {
+    var bytes = [];
+    var i = 8;
+    do {
+    bytes[--i] = int & (255);
+    int = int>>8;
+    } while ( i )
+    return bytes;
 }
 
-//previous, randomness and public_key are hexadecimal strings, round is a string representing an int
+//message takes hex string and int and returns bytes
+function message(msg, round) {
+  var b_msg = hexToBytes(msg);
+  var b_round = intToBytes(round);
+  return b_round.concat(b_msg);
+}
+
+//previous, randomness and pub_key are hexadecimal strings, round is a string representing an int
 function verify_drand(previous, randomness, round, pub_key) {
   var nist = kyber.curve.nist;
   var p256 = new nist.Curve(nist.Params.p256);
-  var message = hexToBytes(previous);
+  var msg = message(previous, round);
   var p = new kyber.pairing.point.BN256G2Point();
   p.unmarshalBinary(hexToBytes(pub_key));
   var sig = hexToBytes(randomness);
-  return kyber.sign.bls.verify(message, p, sig);
+  return kyber.sign.bls.verify(msg, p, sig);
 }
